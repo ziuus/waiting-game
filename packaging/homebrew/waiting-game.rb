0d72ccfc712a2dbfb9022e0a8f750e181f848f47
ziuus <noelyt101@gmail.com>
2026-05-26 23:21:07 +0530

    chore(packaging): update manifests for v0.3.12

--- packaging/homebrew/waiting-game.rb
+++ packaging/homebrew/waiting-game.rb
@@ -1,6 +1,6 @@
 cask "waiting-game" do
-  version "0.3.7"
-  sha256 "481778f104aeac2e703e20088935e280321b3dd29a971c53f9b085499ac81dbc"
+  version "0.3.12"
+  sha256 "8e09ff94f2fed836b17684c877dd5cec2ced6af3510a36e18b56c5a120bb1dbf"
 
   url "https://github.com/ziuus/waiting-game/releases/download/v#{version}/waiting-game_#{version}_aarch64.dmg"
   name "Waiting Game"
